066a5715e37316766c12ed472b287b08ff923ece
Pizza Li <libishengno1@foxmail.com>
2018-11-21 19:55:32 +0800

    added scripts

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils import setup
+from setuptools import setup
 
 setup(
     name='ftp',
     version='1.0',
-    packages=find_packages(),
     author='Pizza Li',
     author_email='libishengno1@foxmail.com',
     description='Simple ftp server and client',
-    scripts=['scripts/ftp-server.py', 'scripts/ftp-cli.py'],
-    install_requires = ['docopt>=0.6.2'],
+    scripts=['scripts/ftp-server', 'scripts/ftp-client'],
+    install_requires=['docopt>=0.6.2', 'PyYAML==3.12'],
 )
